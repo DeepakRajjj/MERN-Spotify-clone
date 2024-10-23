abb0511814ab56594b637962168f8ffde003b940
Deepak Raj <99645724+DeepakRajjj@users.noreply.github.com>
2024-10-23 22:43:25 +0530

    Update PlayerContext.jsx

--- client/src/context/PlayerContext.jsx
+++ client/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
   const seekbg = useRef();
   const seekBar = useRef();
 
-  const url = 'http://localhost:4000';
+  const url = 'https://mern-spotify-clone-backend.onrender.com';
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
 
